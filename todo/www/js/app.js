c37f9e907efdc7350dfecee6fbd20441642aabff
Sujay Khandekar <sskhandek@gmail.com>
2014-08-17 01:00:00 -0700

    Fixed

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -48,25 +48,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
     .state('tab.account', {
       url: '/account',
       views: {
