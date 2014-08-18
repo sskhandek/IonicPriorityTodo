a19eb52e0a15ef09d52081fb26ccdaa83de5ffe1
Sujay Khandekar <sskhandek@gmail.com>
2014-08-18 01:20:12 -0700

    Fixed login etc

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -7,7 +7,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -15,10 +14,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
     .state('signin', {
@@ -57,7 +52,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/sign-in');
+  $urlRouterProvider.otherwise('/tab/mylist');
 
 });
 
